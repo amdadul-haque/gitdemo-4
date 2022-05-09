bccac0e0da2dd7eae08ee5f4a4c5deee3c5bb298
amdadul-haque <amdadul1807107@gmail.com>
2022-05-09 23:37:43 +0600

    main class created

---
+++ master.java
@@ -0,0 +1,13 @@
+public class master{
+  int roll;
+
+  master(int roll){
+    this.roll = roll;
+  }
+  public int getROll(){
+    return roll;
+  }
+  public void setRoll(int roll){
+    this.roll = roll;
+  }
+}
